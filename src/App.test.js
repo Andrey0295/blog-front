5e44e04fbac84c1102e37df24cae5d4683c96084
Andrey0295 <andreevicandrej00@gmail.com>
2021-08-18 15:55:54 +0300

    adds tests for articles-actions

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,9 @@ import articlesReducers from './redux/articles/articles-reducers';
 import articlesActions from './redux/articles/articles-actions';
 
 
+
+const { addArticlesSuccess, changeFilter,editArticlesSuccess,deleteArticlesSuccess } = articlesActions;
+
 test('should return the initial state', () => {
   expect(articlesReducers(undefined, {})).toEqual(
     {
@@ -25,7 +28,7 @@ test('should handle an article being added to a list', () => {
           }],
           filter: ""
         }
-    expect(articlesReducers(previousState, articlesActions.addArticlesSuccess(payload)))
+    expect(articlesReducers(previousState, addArticlesSuccess(payload)))
         .toEqual(expectedState)
 })
 
@@ -36,11 +39,41 @@ test('should to return filter value from changeFilterAction', () => {
         articles: [],
         filter: 'I am test change-filter-payload'
     }
-    expect(articlesReducers({}, articlesActions.changeFilter(payload)))
+    expect(articlesReducers({}, changeFilter(payload)))
         .toEqual(expectedState)
     
+})
 
-
-
+describe('actions', () => {
+  it('should create an action to add an article', () => {
+    const article = {title: 'Test', body: 'Test Test'}
+    const expectedAction = {
+      type: 'articles/addArticlesSuccess',
+      payload: article
+    }
+    expect(addArticlesSuccess(article)).toEqual(expectedAction)
+  })
+  it('should create an action to edit an article', () => {
+      const articleForEdit = {
+          title: 'Test-title after edit', body: 'Test-body after edit'
+      }
+      const expectedAction = {
+          type: 'articles/editArticlesSuccess',
+          payload: articleForEdit
+      }
+  expect(editArticlesSuccess(articleForEdit)).toEqual(expectedAction)    
+  })
+  it('should create an action to delete an article', () => {
+      const articleForDelete = {
+          id: 0,
+          title: 'Will delete',
+          body: 'Test Test'
+       }
+      const expectedAction = {
+          type: 'articles/deleteArticlesSuccess',
+          payload: articleForDelete.id
+       }
+  expect(deleteArticlesSuccess(articleForDelete.id)).toEqual(expectedAction)
+  })
 })
 
